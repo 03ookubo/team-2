4b004a6b145c03b1cbc1ee3b1c393702e9950635
23110001 <23110001@oic-ok.ac.jp>
2024-06-04 10:58:00 +0900

    おみくじ用ダイアログの作成

--- assets/js/js_sojaHighschool.js
+++ assets/js/js_sojaHighschool.js
@@ -1,9 +1,20 @@
 
+
 const OMIKUJI_ARY = ["絶頂", "大吉", "吉", "中吉", "小吉", "末吉", "凶", "大凶", "死亡"];
 
-    const BUTTON = document.getElementById("omikuji"); // idがomikujiの部分を選択
-    BUTTON.addEventListener('click', () => {
-        let random = Math.trunc(Math.random()*9);
-        console.log(random);
-        alert(OMIKUJI_ARY[random]);
-    });
+const BUTTON = document.getElementById("omikuji"); // idがomikujiの部分を選択
+
+BUTTON.addEventListener('click', () => {
+    let random = Math.trunc(Math.random() * OMIKUJI_ARY.length);
+    console.log(random);
+    document.getElementById('omikuji-result').innerText = "今日の運勢は「" + OMIKUJI_ARY[random] + "」 です";
+    showDialog();
+});
+
+//ダイアログ
+function showDialog() {
+    document.getElementById('dialog-overlay').style.display = 'flex';
+}
+function closeDialog() {
+    document.getElementById('dialog-overlay').style.display = 'none';
+}
