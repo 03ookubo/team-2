f946bb72b56fbdf4da87e22ee63298bf30bcd189
23110005 <23110005@oic-ok.ac.jp>
2024-06-04 10:33:47 +0900

    リセットボタンの追加

--- assets/js/school_kasasyouHighSchool.js
+++ assets/js/school_kasasyouHighSchool.js
@@ -1,6 +1,12 @@
+//クリックのカウント
 let count = 0;
 document.getElementById("clickButton").addEventListener("click", function() {
   count++;
+  document.getElementById("clickCount").textContent = count;
+});
+//クリックカウントのリセット
+document.getElementById("resetButton").addEventListener("click", function() {
+  count = 0;
   document.getElementById("clickCount").textContent = count;
 });
 // トップページスライドショー
