fe101ad5c010a48eac46c3301a9b312bf479d810
23110005 <23110005@oic-ok.ac.jp>
2024-06-05 11:27:59 +0900

    班員からのコメントかいた

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,4 +59,14 @@ $(function(){
             return false;
     });
 });
-
+//クリックのカウント
+let count = 0;
+document.getElementById("clickButton").addEventListener("click", function() {
+  count++;
+  document.getElementById("clickCount").textContent = count;
+});
+//クリックカウントのリセット
+document.getElementById("resetButton").addEventListener("click", function() {
+  count = 0;
+  document.getElementById("clickCount").textContent = count;
+});
